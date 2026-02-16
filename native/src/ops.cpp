76f789c14b2496a522243a65e2e9a6d5b2684069
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2026-02-16 23:59:29 +0100

    feat: refactor recursive_apply function to support custom operations

--- native/src/ops.cpp
+++ native/src/ops.cpp
@@ -1,17 +1,22 @@
 #include <vector>
 
-#include "bt/storage.h"
+template <class Op>
+void recursive_apply_tt(int dim, int last_dim,
+                        const std::vector<int64_t>& shape,
+                        const std::vector<float>& a,
+                        const std::vector<float>& b, std::vector<float>& out,
+                        int64_t offset_a, int64_t offset_b, int64_t offset_out,
+                        const std::vector<int64_t>& stride_a,
+                        const std::vector<int64_t>& stride_b,
+                        const std::vector<int64_t>& stride_out, Op op) {
+  if (shape.size() == 0) {
+    out[offset_out] = op(a[offset_a], b[offset_b]);
+    return;
+  }
 
-void recursive_apply(int dim, int last_dim, const std::vector<int64_t>& shape,
-                     const std::vector<float>& a, const std::vector<float>& b,
-                     std::vector<float>& out, int64_t offset_a,
-                     int64_t offset_b, int64_t offset_out,
-                     const std::vector<int64_t>& stride_a,
-                     const std::vector<int64_t>& stride_b,
-                     const std::vector<int64_t>& stride_out) {
   if (dim == last_dim) {
     for (int64_t i = 0; i < shape[dim]; i++) {
-      out[offset_out] = a[offset_a] + b[offset_b];
+      out[offset_out] = op(a[offset_a], b[offset_b]);
       offset_a += stride_a[dim];
       offset_b += stride_b[dim];
       offset_out += stride_out[dim];
@@ -21,7 +26,7 @@ void recursive_apply(int dim, int last_dim, const std::vector<int64_t>& shape,
 
   for (int64_t i = 0; i < shape[dim]; i++) {
     recursive_apply(dim + 1, last_dim, shape, a, b, out, offset_a, offset_b,
-                    offset_out, stride_a, stride_b, stride_out);
+                    offset_out, stride_a, stride_b, stride_out, op);
     offset_a += stride_a[dim];
     offset_b += stride_b[dim];
     offset_out += stride_out[dim];
