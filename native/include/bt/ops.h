17c0c5a4a82e57708d56ac029f6f08ab1684cbcf
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2026-02-16 23:58:35 +0100

    feat: implement basic arithmetic operations (Add, Sub, Mul, Div) in ops namespace

---
+++ native/include/bt/ops.h
@@ -0,0 +1,16 @@
+namespace bt::ops {
+
+struct Add {
+  float operator()(float x, float y) const noexcept { return x + y; }
+};
+struct Sub {
+  float operator()(float x, float y) const noexcept { return x - y; }
+};
+struct Mul {
+  float operator()(float x, float y) const noexcept { return x * y; }
+};
+struct Div {
+  float operator()(float x, float y) const noexcept { return x / y; }
+};
+
+}  // namespace bt::ops
