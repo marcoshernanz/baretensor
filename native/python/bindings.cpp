d00c821a02724e923b9f9440fa2d3d13d9d87f91
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2026-02-17 01:24:19 +0100

    refactor: reorganize Tensor class method bindings for improved readability

--- native/python/bindings.cpp
+++ native/python/bindings.cpp
@@ -1,4 +1,5 @@
 #include <nanobind/nanobind.h>
+#include <nanobind/operators.h>
 #include <nanobind/stl/vector.h>
 
 #include "bt/tensor.h"
@@ -8,7 +9,20 @@ namespace nb = nanobind;
 NB_MODULE(_C, m) {
   m.doc() = "BareTensor native extension (bootstrap)";
 
-  nb::class_<bt::Tensor>(m, "Tensor").def_ro("shape", &bt::Tensor::shape);
+  nb::class_<bt::Tensor>(m, "Tensor")
+      .def_ro("shape", &bt::Tensor::shape)
+      .def_ro("strides", &bt::Tensor::strides)
+      .def("dim", &bt::Tensor::dim)
+      .def("numel", &bt::Tensor::numel)
+      .def("is_contiguous", &bt::Tensor::is_contiguous)
+      .def(nb::self + nb::self)
+      .def(nb::self + float())
+      .def(nb::self - nb::self)
+      .def(nb::self - float())
+      .def(nb::self * nb::self)
+      .def(nb::self * float())
+      .def(nb::self / nb::self)
+      .def(nb::self / float());
 
   m.def("full", &bt::full);
   m.def("zeros", &bt::zeros);
