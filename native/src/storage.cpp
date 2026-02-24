d0f1a451883b33d19ad2f98ed4e4fafd0b85de66
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2026-02-24 01:09:16 +0100

    refactor: update Storage constructor to accept const reference of vector<float> for better performance

--- native/src/storage.cpp
+++ native/src/storage.cpp
@@ -6,7 +6,7 @@ namespace bt {
 
 Storage::Storage(int64_t size) : data(size) {}
 
-Storage::Storage(std::vector<float>& data) { this->data = std::move(data); }
+Storage::Storage(const std::vector<float>& data) : data(data) {}
 
 void Storage::fill(float fill_value) {
   std::fill(data.begin(), data.end(), fill_value);
