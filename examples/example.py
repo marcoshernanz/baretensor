0aabc2c27520fab0c9cd00ff2bbec13acebd5387
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2026-02-24 01:10:28 +0100

    refactor: update example.py to use numpy array for tensor initialization

--- examples/example.py
+++ examples/example.py
@@ -1,10 +1,16 @@
 # %%
+
 import bt
 import torch
+import numpy as np
+
+ndarray = np.arange(10, dtype=np.float32)
+
+print(ndarray)
 
 
-t1 = torch.ones([2, 3, 4])
+t1 = torch.tensor(ndarray)
 print(t1)
 
-t2 = bt.ones([2, 3, 4])
+t2 = bt.tensor(ndarray)
 print(t2)
