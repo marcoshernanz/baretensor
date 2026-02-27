da0de09da6e8df675ff1b878cce6bc1993b10d19
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2026-02-27 19:28:33 +0100

    feat: add sum method to Tensor class and remove matmul from exports

--- src/bt/__init__.py
+++ src/bt/__init__.py
@@ -1,5 +1,5 @@
 """BareTensor Python package (bootstrap)."""
 
-from ._C import Tensor, full, matmul, ones, tensor, zeros  # pylint: disable=no-name-in-module
+from ._C import Tensor, full, ones, tensor, zeros  # pylint: disable=no-name-in-module
 
-__all__ = ["Tensor", "full", "zeros", "ones", "matmul", "tensor"]
+__all__ = ["Tensor", "full", "zeros", "ones", "tensor"]
