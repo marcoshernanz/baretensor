fix: update mask creation to specify boolean dtype and adjust softmax axis in attention calculation

notebooks/attention.py
@@ -34,8 +34,8 @@ K = E @ Wk
 V = E @ Wv
 
 x = (Q @ K.T) / jnp.sqrt(ATTENTION_DIM)
-mask = jnp.triu(jnp.ones((SEQUENCE_LEN, SEQUENCE_LEN)), k=1)
+mask = jnp.triu(jnp.ones((SEQUENCE_LEN, SEQUENCE_LEN), dtype=bool), k=1)
 masked = jnp.where(mask, -jnp.inf, x)
-normalized = jnn.softmax(masked, 1)
+normalized = jnn.softmax(masked, -1)
 attention = normalized @ V
 output = attention @ Wo