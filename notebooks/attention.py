fix: separate value and position embeddings in attention calculation

notebooks/attention.py
@@ -18,16 +18,19 @@ tokens = jnp.array([char_to_id[c] for c in sequence])
 # %%
 
 key = jax.random.key(1337)
-key, embedding_key, Wq_key, Wk_key, Wv_key, Wo_key = jax.random.split(key, 6)
+key, embedding_key, position_key, Wq_key, Wk_key, Wv_key, Wo_key = jax.random.split(key, 7)
 
-embeddings = jax.random.normal(embedding_key, (VOCAB_SIZE, EMBEDDING_DIM))
+value_embeddings = jax.random.normal(embedding_key, (VOCAB_SIZE, EMBEDDING_DIM))
+position_embeddings = jax.random.normal(position_key, (VOCAB_SIZE, EMBEDDING_DIM))
 Wq = jax.random.normal(Wq_key, (EMBEDDING_DIM, ATTENTION_DIM))
 Wk = jax.random.normal(Wk_key, (EMBEDDING_DIM, ATTENTION_DIM))
 Wv = jax.random.normal(Wv_key, (EMBEDDING_DIM, ATTENTION_DIM))
 Wo = jax.random.normal(Wo_key, (ATTENTION_DIM, EMBEDDING_DIM))
 
+
 # %%
 
+embeddings = position_embeddings + value_embeddings
 E = embeddings[tokens]
 Q = E @ Wq
 K = E @ Wk