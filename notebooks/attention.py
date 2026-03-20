fix: correct mask creation in attention calculation to use lower triangular matrix

notebooks/attention.py
@@ -33,9 +33,9 @@ K = E @ Wk
 V = E @ Wv
 
 x = (Q @ K.T) / jnp.sqrt(ATTENTION_DIM)
-mask = jnp.tril(jnp.full((SEQUENCE_LEN, SEQUENCE_LEN), -1)) + 1
+mask = jnp.tril(jnp.ones((SEQUENCE_LEN, SEQUENCE_LEN)))
 masked = jnp.where(mask, -jnp.inf, x)
 normalized = jnn.softmax(masked, 1)
 attention = normalized @ V
 
-attention
+mask