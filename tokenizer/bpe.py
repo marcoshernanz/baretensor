eb88fd98129522fea7982f84d07ef7fcb876d995
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2026-03-25 20:48:37 +0100

    feat: implement most_frequent_pair function for BPE tokenization

--- tokenizer/bpe.py
+++ tokenizer/bpe.py
@@ -6,3 +6,14 @@ text = DATA_PATH.read_text(encoding="utf-8")[:100000]
 target_vocab_size = 266
 
 words = [[c for c in f" {word}".encode()] for word in text.split(" ")]
+
+# %%
+
+
+def most_frequent_pair(words: list[list[int]]) -> tuple[int, int]:
+    freq = dict()
+    for word in words:
+        for pair in zip(word, word[1:]):
+            freq[pair] = freq[pair] + 1 if pair in freq else 1
+
+    return max(freq.items(), key=lambda item: item[1])[0]
