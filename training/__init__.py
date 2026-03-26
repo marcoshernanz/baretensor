7e1cb510f062feeca51681c0acf797a61d341162
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2026-03-26 16:23:55 +0100

    refactor: remove run_from_resume from exports in training module

--- training/__init__.py
+++ training/__init__.py
@@ -2,12 +2,10 @@ from training.config import TrainingConfig
 from training.config import load_config
 from training.runner import RunResult
 from training.runner import run_from_config
-from training.runner import run_from_resume
 
 __all__ = [
     "RunResult",
     "TrainingConfig",
     "load_config",
     "run_from_config",
-    "run_from_resume",
 ]
